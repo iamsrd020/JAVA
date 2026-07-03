52c391daeef134eec8af22e6f162c89a3a11187b
DARSHAN S R <darshansr403@gmail.com>
2026-07-03 23:14:15 +0530

    Started with reservedWords in java

---
+++ reservedWords.java
@@ -0,0 +1,37 @@
+/* 🟢🟢Reserved Words in JAVA 🟢🟢 */
+
+/*
+ * In JAVA some words are rerserved to represnt some meaning and funactionlaty
+ * such type of worsd are caleld Reserved words
+ */
+
+/*
+ * There are 53 reserved words in JAVA , where 50 are keywords and 3 are
+ * Reserved literals (eg: true, false, null)
+ */
+
+/*
+ * In 50 keywords 48 are used keywords and 2 are not used keywords(eg: const,
+ * goto)
+ * eg: for used keywords: if, else, while, do, switch, case, break, continue,
+ * return, try, catch, finally, throw, throws, synchronized, volatile,
+ * transient,
+ * native, strictfp, assert, instanceof, new, this, super, class, interface,
+ * extends, implements, package, import, public, private, protected, static 🚀🚀
+ */
+
+/* keywords for Data types */
+public class reservedWords {
+    public static void main(String[] args) {
+        int x = 10; // int is a reserved word for integer data type
+        float y = 10.5f; // float is a reserved word for floating-point data type
+        double z = 10.5; // double is a reserved word for double-precision floating-point data type
+        char c = 'A'; // char is a reserved word for character data type
+        boolean b = true; // boolean is a reserved word for boolean data type
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
