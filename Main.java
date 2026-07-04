161b1498f1b73e5f40064358c1619cdb8e73616f
DARSHAN S R <darshansr403@gmail.com>
2026-07-05 00:03:45 +0530

    Void Example in JAVA

---
+++ Main.java
@@ -0,0 +1,22 @@
+/* Example for Void keyword in JAVA */
+
+// public class Main {
+//     public static void greet() {
+//         System.out.println("Hello, I am Darshan SR, a BACKEND DEVELOPER 💻");
+//     }
+
+//     public static void main(String[] args) {
+//         greet(); // Calling the method
+//     }
+// }
+
+public class Main {
+    public static void DisplayName(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        DisplayName("Darshan SR");
+        DisplayName("Harshitha CM");
+    }
+}
