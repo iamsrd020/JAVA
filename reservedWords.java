1c61f8f42e606b2494b35e13e8cb6e73b2eef3d1
DARSHAN S R <darshansr403@gmail.com>
2026-07-04 23:52:30 +0530

    Java learning

--- reservedWords.java
+++ reservedWords.java
@@ -21,17 +21,57 @@
  */
 
 /* keywords for Data types */
-public class reservedWords {
+public class ReservedWords {
     public static void main(String[] args) {
         int x = 10; // int is a reserved word for integer data type
         float y = 10.5f; // float is a reserved word for floating-point data type
         double z = 10.5; // double is a reserved word for double-precision floating-point data type
         char c = 'A'; // char is a reserved word for character data type
         boolean b = true; // boolean is a reserved word for boolean data type
+        byte bt = 100; // byte is a reserved word for byte data type
+        short s = 1000; // short is a reserved word for short data type
+        long l = 100000; // long is a reserved word for long data type
+        char ch = 'B'; // char is a reserved word for character data type
         System.out.println(x);
         System.out.println(y);
         System.out.println(z);
         System.out.println(c);
         System.out.println(b);
+        System.out.println(bt);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(ch); // these above listed are 8 keywords for data types in JAVA
     }
 }
+
+/*
+ * Keywords for Flow Control Eg: if, else, switch, case, default, while, do,
+ * for, break, continue, return i.e., there are 11 keywords for flow control in
+ * JAVA
+ */
+
+/*
+ * keywords for modifiers eg: public, private, protected, static, final,
+ * abstract, synchronized, volatile, transient, native, strictfp(introduced in
+ * JAVA 1.2), assert(introduced in JAVA 1.4) i.e., there are 12 keywords for
+ * modifiers in JAVA
+ */
+
+/*
+ * Keywords for Exception Handling eg: try, catch, finally, throw, throws,
+ * assert(inroduced in JAVA 1.4) i.e., there are 6 keywords for exception
+ * handling in JAVA
+ */
+
+/*
+ * Class related key words eg: class, interface, extends, implements , package,
+ * import
+ */
+
+/* Object related keywords eg: instanceof, new, super, and this */
+
+/* Void return type keyword eg: void */
+/*
+ * In java return type is mandatory, if a method wont return anything then we
+ * have to decalre that method with void return type
+ */
