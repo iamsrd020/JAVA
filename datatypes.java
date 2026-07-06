e0f67f7359f780067c0e57c370e172c3ac21c190
DARSHAN S R <darshansr403@gmail.com>
2026-07-06 23:08:38 +0530

    Started with DATATYPES in Java🍵

---
+++ datatypes.java
@@ -0,0 +1,29 @@
+/* 🟢🟢🟢🟢🟢 DATA TYPES IN JAVA 🟢🟢🟢🟢🟢 */
+
+/*
+ * 1) In JAVA, every variable and every expression has some data type
+ * 2) Each and every data type is cleraly defined in JAVA
+ * 3) Every assignment should be checked by compiler for type compatibility
+ * 
+ * 
+ * so because of the above points, JAVA is called as a strongly typed
+ * programming language
+ */
+
+/*
+ * 1) When compared with old languages like C, C++, yes java has more object
+ * orientred features or nature.
+ * 
+ * 2) But, when Java is considred alone, without any relative comparsion, many
+ * Object oriented features are not supporting JAVA.
+ */
+
+/*
+ * Important Note: Interview scenario
+ * 
+ * @@@@) JAVA is not considered as a pure object oriented programming language
+ * because several OOP features are not supported in JAVA, like 1) Multiple
+ * Inheritance 2) Operator Overloading 3) Pointers 4) Friend function 5) Friend
+ * class
+ * 
+ */
